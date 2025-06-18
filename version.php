9ff7fc7e79078e254e55cee20e81e8384a7bc422
Marcin Calka <6230943+marcinca@users.noreply.github.com>
2025-06-18 14:34:26 +0100

    DIG-7417 Tweak garding table header gap for Moodle 404

--- version.php
+++ version.php
@@ -26,7 +26,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 // This is the version of the plugin.
-$plugin->version = 2025051301;
+$plugin->version = 2025062301;
 $plugin->release = '405.1.0';
 $plugin->maturity = MATURITY_BETA;
 
