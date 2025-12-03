f53dee5f4e1bcb5e3f8e6f616e376e6ee698ff93
Marcin Calka <6230943+marcinca@users.noreply.github.com>
2025-12-03 12:17:42 +0000

    DIG-7851
    1. Course pagination added back (chelper init removed)2
    2. CSS header height and minimum iframe height tweaks

--- version.php
+++ version.php
@@ -26,7 +26,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 // This is the version of the plugin.
-$plugin->version = 2025110601;
+$plugin->version = 2025120301;
 $plugin->release = '405.1.0';
 $plugin->maturity = MATURITY_BETA;
 
