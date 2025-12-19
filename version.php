d7a37f77aa5309b0798dac6d07364bf2006816a7
Marcin C. <6230943+marcinca@users.noreply.github.com>
2025-12-19 12:46:54 +0000

    DIG-7636 Add paging bar with nhsuk class (#110)

--- version.php
+++ version.php
@@ -26,7 +26,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 // This is the version of the plugin.
-$plugin->version = 2025120301;
+$plugin->version = 2025121901;
 $plugin->release = '405.1.0';
 $plugin->maturity = MATURITY_BETA;
 
